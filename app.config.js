d54b3ea61f4b0e24cce51a3050a0abb27610c987
alexandroskazalis <alexandros.kazalis@hotmail.com>
2025-08-29 20:30:38 +0200

    Added project id hardcoded instead of retrieving from .env since it is not allowed.
    Retrieving Firebase .env secrets by using Constants

--- app.config.js
+++ app.config.js
@@ -36,6 +36,13 @@ export default ({ config }) => {
         GOOGLE_EXPO_CLIENT_ID: process.env.GOOGLE_EXPO_CLIENT_ID,
         GOOGLE_ANDROID_CLIENT_ID: process.env.GOOGLE_ANDROID_CLIENT_ID,
         GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        FIREBASE_MEASURMENT_ID: process.env.FIREBASE_MEASURMENT_ID,
         eas: {
           projectId: "73e5ba86-8f70-4d0d-b33c-8bd4c8691ddc"
         }
