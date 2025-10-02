482fea6366b88a573169d1ba14ec7a6ecd7cd9fe
alexandroskazalis <alexandros.kazalis@hotmail.com>
2025-10-02 05:29:15 +0200

    Updated Settings screen and added a danger zone for user to be able to delete their own account.
    Created a delete modal for user to confirm delete or cancel.

---
+++ src/components/DeleteAccountConfirmModal.tsx
@@ -0,0 +1,57 @@
+import { ActivityIndicator, Modal, Text, TouchableOpacity, View } from "react-native";
+import { Layout } from "../styles/Layout";
+import { useAppTheme } from "../hooks/useAppTheme";
+import { Typography } from "../styles/Typography";
+import { Colors } from "../styles/colors";
+
+interface Props {
+    visible: boolean;
+    onClose: () => void;
+    onConfirm: () => void;
+    isLoading: boolean;
+}
+
+const DeleteAccountConfirmModal: React.FC<Props> = ({visible, onClose, onConfirm, isLoading}) => {
+    const theme = useAppTheme();
+
+    return (
+        <Modal
+            animationType="fade"
+            transparent
+            visible={visible}
+            onRequestClose={onClose}
+        >
+            <View style={[Layout.container, {justifyContent: "center", alignItems: "center", backgroundColor: 'rgba(0,0,0,0.5)'}]}>
+                <View style={{backgroundColor: theme.auth.background, padding: 20, borderRadius: 10, borderWidth: 1, borderColor: 'red' }}>
+                    <Text style={[Typography.title, { color: 'red', textAlign: 'center', marginBottom: 10 }]}>
+                        Are you sure you want to delete your account?
+                    </Text>
+                    <Text style={[Typography.body, { textAlign: 'center', marginBottom: 20 }]}>
+                        This action is irreversible and will permanently delete your data.
+                    </Text>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                        <TouchableOpacity
+                            style={[Layout.button, { backgroundColor: Colors.error, width: 100 }]}
+                            onPress={onConfirm}
+                        >
+                            {isLoading ? (
+                                <ActivityIndicator size="small" color="white" />
+                            ) : (
+                                <Text style={Layout.buttonText}>Delete</Text>
+                            )}
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={[Layout.button, { backgroundColor: Colors.surface, borderWidth: 1, width: 100 }]}
+                            onPress={onClose}
+                        >
+                            <Text style={[Layout.buttonText, { color: 'black' }]}>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    )
+}
+
+export default DeleteAccountConfirmModal;
--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Switch, ScrollView } from "react-native"; 
+import { View, Text, Switch, ScrollView, TouchableOpacity } from "react-native"; 
 import { useAppTheme } from "../hooks/useAppTheme"; 
 import { useTheme } from "../context/ThemeContext"; 
 import { Typography } from "../styles/Typography"; 
@@ -6,10 +6,46 @@ import Navbar from "../components/Navbar";
 import Divider from "../components/Divider"; 
 import { Layout } from "../styles/Layout"; 
 import { SafeAreaView } from 'react-native-safe-area-context';
+import DeleteAccountConfirmModal from "../components/DeleteAccountConfirmModal";
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { deleteUser } from "../services/UserService";
+import * as SecureStore from "expo-secure-store";
+import Toast from "react-native-root-toast";
 
 const SettingsScreen = () => { 
     const theme = useAppTheme(); 
     const { isDark, toggleTheme } = useTheme(); 
+    const [isDeleteConfirmVisible, setIsDeleteConfirmVisible] = useState(false);
+    const {user, logout} = useAuth();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleDeleteAccount = async () => {
+        if(!user?.id) {
+            return;
+        }
+
+        try {
+            setIsLoading(true)
+            await deleteUser(user.id);
+            await SecureStore.deleteItemAsync("streak");
+            await logout();
+
+            Toast.show("Account deleted successfully.", {
+                duration: Toast.durations.LONG,
+                position: Toast.positions.TOP,
+            });
+        } catch (error) {
+            console.error("Delete account failed", error);
+            Toast.show("Failed to delete account. Try again.", {
+                duration: Toast.durations.LONG,
+                position: Toast.positions.TOP,
+            });
+        } finally {
+            setIsLoading(false)
+            setIsDeleteConfirmVisible(false);
+        }
+    }
     
     return ( 
         <SafeAreaView style={{flex: 1, backgroundColor: theme.auth.navbar}}> 
@@ -22,13 +58,30 @@ const SettingsScreen = () => {
                         <Text style={[Typography.body, { color: theme.auth.text, marginRight: 10 }]}> Dark Mode </Text> 
                         <Switch value={isDark} onValueChange={toggleTheme} trackColor={{ false: '#767577', true: '#81b0ff' }} thumbColor={isDark ? "#59512e" : '#FAF3E0'} /> 
                     </View> 
-                    <Divider /> 
                     
+                    <Divider /> 
                     <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 10}}> 
                         <Text style={[Typography.body, { color: theme.auth.text, marginRight: 10 }]}> Notifications </Text> 
                     </View> 
+
+                    <View style={{ marginTop: 40, borderTopWidth: 1, borderColor: 'red', paddingTop: 20 }}>
+                        <Text style={[Typography.label, { color: 'red', fontWeight: 'bold', fontSize: 18 }]}>Danger Zone</Text>
+                        <TouchableOpacity
+                            style={[Layout.button, { backgroundColor: '#ff4d4f', borderColor: 'darkred', borderWidth: 1, marginTop: 10 }]}
+                            onPress={() => setIsDeleteConfirmVisible(true)}
+                        >
+                            <Text style={{color: 'white'}}>Delete My Account</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View> 
             </ScrollView> 
+
+            <DeleteAccountConfirmModal
+                visible={isDeleteConfirmVisible}
+                onClose={() => setIsDeleteConfirmVisible(false)}
+                onConfirm={handleDeleteAccount}
+                isLoading={isLoading}
+            />
         </SafeAreaView> 
     ) 
 } 
